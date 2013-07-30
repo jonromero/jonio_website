17a695b40352cd2a1e294c25472bd9a0e336b286
Jon Vlachogiannis <darksun4@gmail.com>
2013-07-31 00:47:11 +0300

    going to master

---
+++ content/pages/about.md
@@ -0,0 +1,5 @@
+Title: About
+Tags: about
+Summary: Who am I
+
+Coming soon (and is going to be awesome!)
---
+++ pelicanconf.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'Jon V <h2>BigData. Startups. Trading.</h2>'
+SITENAME = u'Jon.IO'
+SITEURL = ''
+
+TIMEZONE = 'Europe/Paris'
+
+DEFAULT_LANG = u'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+
+# Social widget
+SOCIAL = (('Twitter', 'http://twitter.com/jonromero'),
+          ('LinkedIn', 'http://www.linkedin.com/in/johnvlachoyiannis'),
+          ('Github', 'http://github.com/jonromero/'),
+          ('Bitbucket', 'http://bitbucket.org/jonromero/'),)
+
+# Blogroll
+LINKS =  ()
+
+DEFAULT_PAGINATION = False
+THEME = "pelican-svbtle-master"
+DEFAULT_DATE = "fs"
+
+TWITTER_USERNAME = "jonromero"
+# Uncomment following line if you want document-relative URLs when developing
+RELATIVE_URLS = True
