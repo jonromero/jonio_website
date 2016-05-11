8e4724b5ce3a1264c9c03e6658302a2d583ea47a
Jon Vlachoyiannis <darksun4@gmail.com>
2016-05-10 18:19:42 -0700

    Update prices.js

--- prices.js
+++ prices.js
@@ -1,8 +1,8 @@
 window["jonromero/jonio_website:master"]({
   "prices": {
     "EURUSD": {
-      "August": "102.1",
-      "September": "11.2",
-      "February": "1.1"
+      "August": 102.1,
+      "September": 11.2,
+      "February": 1.1
     }
 }});
