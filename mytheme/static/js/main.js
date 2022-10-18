d6ef24345eb464239950a53edb33e4e039b8aa4c
Jon V <jon@agentrisk.com>
2022-10-17 20:42:28 -0700

    metaverse 2022

--- mytheme/static/js/main.js
+++ mytheme/static/js/main.js
@@ -7,4 +7,19 @@ function toggleMenus() {
     }
 }
 
+function text_rotation() {
+    return {
+        animated_text: 'epic',
 
+        rotate_words() {
+            var words = ['Startups', 'Web3', 'Hackathons', 'Metaverse', 'Cybersecutity', 'Biohacking', 'DAOs', 'Crypto'],
+                currentWord = 0;
+
+            setInterval(() => {
+                if (currentWord > words.length - 1) currentWord = 0;
+                this.animated_text = words[currentWord];
+                currentWord++;
+            }, 1500);
+        }
+    }
+}
